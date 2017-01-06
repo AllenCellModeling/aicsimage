3c29126290127032c74b4149a8409e5376d2ab9b
Zach Crabtree <zacharyc@alleninstitute.org>
2017-01-06 09:47:01 -0800

    Remove ometif_test_output as it is randomly generated. Update OmeTifWriter tests to remove the generated file after testing.

--- test/testOmeTifWriter.py
+++ test/testOmeTifWriter.py
@@ -16,11 +16,15 @@ class OmeTifWriterTestGroup(unittest.TestCase):
     def setUpClass(cls):
         aicsimagetools.init()
         cls.dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
+        cls.file = os.path.join(cls.dir_path, 'ometif_test_output.ome.tif')
         cls.image = np.random.rand(1, 40, 3, 128, 256)
-        cls.writer = omeTifWriter.OmeTifWriter(os.path.join(cls.dir_path, 'ometif_test_output.ome.tif'))
+        cls.writer = omeTifWriter.OmeTifWriter(cls.file)
+        if not os.path.isfile(cls.file):
+            open(cls.file, 'a').close()
 
     @classmethod
     def tearDownClass(cls):
+        os.remove(cls.file)
         aicsimagetools.close()
 
     """
@@ -29,13 +33,13 @@ class OmeTifWriterTestGroup(unittest.TestCase):
     def test_writerShapeComparison(self):
         self.writer.save(self.image, overwrite_file=True)
 
-        with omeTifReader.OmeTifReader(os.path.join(self.dir_path, 'ometif_test_output.ome.tif')) as test_output_reader:
+        with omeTifReader.OmeTifReader(self.file) as test_output_reader:
             output = test_output_reader.load()
 
         self.assertEqual(output.shape, self.image.shape)
 
     """
-    Test to check if save() will only accept 3,4,5 dimensions for data
+    Test to check if save() will only accept 3, 4, 5 dimensions for data
     """
     def test_loadAssertionError(self):
         image_to_save = np.ones((1, 2, 3, 4, 5, 6))
@@ -46,7 +50,7 @@ class OmeTifWriterTestGroup(unittest.TestCase):
     Test to check if save() can overwrite a file
     """
     def test_overwriteFile(self):
-        with omeTifWriter.OmeTifWriter(os.path.join(self.dir_path, 'ometif_test_output.ome.tif')) as writer:
+        with omeTifWriter.OmeTifWriter(self.file) as writer:
             writer.save(self.image, overwrite_file=True)
 
     """
@@ -54,5 +58,5 @@ class OmeTifWriterTestGroup(unittest.TestCase):
     """
     def test_dontOverwriteFile(self):
         with self.assertRaises(Exception):
-            with omeTifWriter.OmeTifWriter(os.path.join(self.dir_path, 'ometif_test_output.ome.tif')) as writer:
+            with omeTifWriter.OmeTifWriter(self.file) as writer:
                 writer.save(self.image)
