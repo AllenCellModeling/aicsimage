c7e89c7cf5df75b7a251124290601b1a4b4734d4
zacharyc <zacharyc@alleninstitute.org>
2017-05-01 12:32:14 -0700

    initial texture atlas generation logic (all unit tests still fail)

--- aicsimage/processing/textureAtlas.py
+++ aicsimage/processing/textureAtlas.py
@@ -1,1 +1,101 @@
+# author: Zach Crabtree zacharyc@alleninstitute.org
+
+import math as m
+import numpy as np
+import os
+from aicsimage.io.pngWriter import PngWriter
+
+from aicsImage import AICSImage
+# class TextureAtlas:
+#
+#     def __init__(self, im, outpath, prefix, max_edge, pack_order):
+#         self.image_stack = im
+#         self.outpath = outpath
+#         self.prefix = prefix
+#         self.max_edge = max_edge
+#         self.pack_order = pack_order
+
+def _atlas_single_channel(im, channel, tile_width, tile_height, stack_height):
+    # maintain aspect ratio of images
+    # initialize atlas with one row of all slices
+    atlas_width = tile_width * stack_height
+    atlas_height = tile_height
+    ratio = float(atlas_width) / float(atlas_height)
+    # these next steps attempt to optimize the atlas into a square shape
+    # TODO - there must be a way to do this with a single calculation
+    for r in range(2, stack_height):
+        new_rows = m.ceil(float(stack_height) / r)
+        adjusted_width = int(tile_width * new_rows)
+        adjusted_height = int(tile_height * r)
+        new_ratio = float(max(adjusted_width, adjusted_height)) / float(min(adjusted_width, adjusted_height))
+        if new_ratio < ratio:
+            ratio = new_ratio
+            atlas_width = adjusted_width
+            atlas_height = adjusted_height
+        else:
+            # we've found the rows and columns that make this the most square image
+            break
+
+    cols = int(atlas_width // tile_width)
+    rows = int(atlas_height // tile_height)
+    atlas = np.zeros((atlas_width, atlas_height))
+    i = 0
+    for row in range(rows):
+        top_bound, bottom_bound = (tile_height * row), (tile_height * (row + 1))
+        for col in range(cols):
+            if i < stack_height:
+                left_bound, right_bound = (tile_width * col), (tile_width * (col + 1))
+                atlas[left_bound:right_bound, top_bound:bottom_bound] = im.get_image_data("XY", Z=i, C=channel)
+                i += 1
+
+    return atlas
+
+def generate_texture_atlas(im, outpath="text_atlases/", prefix="atlas", max_edge=2048, pack_order=None):
+    """
+
+    :param im: aicsImage object
+    :param outpath: string containing directory path to save images in
+    :param prefix: all atlases will be saved with this prefix and append _x for each atlas for the image
+    :param max_edge: this designates the largest side in the texture atlas
+    :param pack_order: a 2d list that contains what channel in the image should be saved to the RGBA values in the
+                       final png. for example, a 7 channel image might be saved like [[0, 1, 2, 3], [4, 5], [6]]
+                       where the first texture atlas will code channel 0 as r, channel 1 as g, and so on.
+    :return:
+    """
+    if not isinstance(im, AICSImage):
+        raise ValueError("Texture atlases can only be generated with AICSImage objects!")
+    if pack_order is None:
+        # if no pack order is specified, pack 4 channels per png and move on
+        channel_list = [c for c in range(im.shape[1])]
+        pack_order = [channel_list[x:x+4] for x in xrange(0, len(channel_list), 4)]
+    png_count = 0
+    for png in pack_order:
+        if len(png) > 4:
+            raise ValueError("An atlas with more than 4 channels ({}) cannot be created!".format(png))
+        for channel in png:
+            if channel > im.shape[1]:
+                raise IndexError("A channel with value {} is out-of-bounds in the AICSImage object!".format(channel))
+        stack_height, tile_height, tile_width = im.shape[2], im.shape[3], im.shape[4]
+        layer_list = []
+        for channel in png:
+            xy_layer = _atlas_single_channel(im, channel, tile_width, tile_height, stack_height)
+            layer_list.append(xy_layer)
+        atlas_width, atlas_height = layer_list[0].shape[0], layer_list[0].shape[1]
+        # atlas is a CYX png
+        atlas = np.ndarray((len(png), atlas_height, atlas_width))
+        i = 0
+        for layer in layer_list:
+            atlas[i] = layer.transpose(1, 0)
+            i += 1
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+        full_path = os.path.join(outpath, prefix + "_" + str(png_count) + ".png")
+        with PngWriter(full_path, overwrite_file=True) as writer:
+            writer.save(atlas)
+        png_count += 1
+
+
+
+
+
 
--- test/testTextureAtlas.py
+++ test/testTextureAtlas.py
@@ -3,6 +3,7 @@
 import unittest
 
 from aicsimage.processing.aicsImage import AICSImage
+from aicsimage.processing.textureAtlas import generate_texture_atlas
 
 
 class TextureAtlasTestGroup(unittest.TestCase):
@@ -13,7 +14,8 @@ class TextureAtlasTestGroup(unittest.TestCase):
         max_edge = 2048
         # act
         atlas = generate_texture_atlas(im=image, outpath="img/atlas/",
-                                       prefix="atlas", max_edge=max_edge)
+                                       prefix="test_Sizing", max_edge=max_edge,
+                                       pack_order=[[3, 2, 1, 0], [4]])
         atlas_maxedge = max(atlas.shape[0], atlas.shape[1])
         # assert
         self.assertEqual(atlas_maxedge, max_edge)
@@ -23,7 +25,7 @@ class TextureAtlasTestGroup(unittest.TestCase):
         image = AICSImage("img/img40_1.ome.tif")
         packing_list = [[0], [1, 2], [3, 4], [5, 6]]
         # act
-        atlas = generate_texture_atlas(image, packing=packing_list)
+        atlas = generate_texture_atlas(image, prefix="test_pickChannels", pack_order=packing_list)
         # returns as dict
         metadata = atlas.get_metadata()
         # returns list of dicts
@@ -36,11 +38,11 @@ class TextureAtlasTestGroup(unittest.TestCase):
         # arrange
         image = AICSImage("img/img40_1.ome.tif")
         packing_list = [[0], [1, 2], [3, 4], [5, 6]]
-        output_dir = "img/atlas"
+        output_dir = "img/atlas/"
         prefix = "atlas"
         # act
-        atlas = generate_texture_atlas(image, outpath=output_dir, fileprefix=prefix,
-                                       packing=packing_list)
+        atlas = generate_texture_atlas(image, outpath=output_dir, prefix=prefix,
+                                       pack_order=packing_list)
         # assert
         metadata = atlas.get_metadata()
         self.assertTrue(all(key in metadata for key in ("rows", "cols", "width", "height", "images")))
