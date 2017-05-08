7f8cc995c42ab0936fab31a2c660efa40d9521ab
zacharyc <zacharyc@alleninstitute.org>
2017-05-08 12:05:06 -0700

    add image cropping logic
    still working on filtering logic

--- aicsimage/processing/segmentation/nucleusSegmentation.py
+++ aicsimage/processing/segmentation/nucleusSegmentation.py
@@ -1,6 +1,8 @@
 # author: Zach Crabtree zacharyc@alleninstitute.org
 
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from skimage.filters import threshold_otsu
 
 def fill_nucleus_segmentation(cell_index_img, nuc_index_img, nuc_original_img):
     """
@@ -15,7 +17,27 @@ def fill_nucleus_segmentation(cell_index_img, nuc_index_img, nuc_original_img):
         cell_indices = np.where(cell_index_img == cell_value)
         # if a cell exists with this cell_value
         if len(cell_indices[0]) > 0:
-            y_index, x_index, z_index = np.unravel_index(cell_index_img.shape, cell_indices)
+            z_indices, y_indices, x_indices = cell_indices[0], cell_indices[1], cell_indices[2]
 
+            # creates buffer of 10 pixels/slices around the image, or the boundaries of the original image
+            x_slice = slice(max(1, -10+min(x_indices)), min(10+max(x_indices), cell_index_img.shape[2]))
+            y_slice = slice(max(1, -10+min(y_indices)), min(10+max(y_indices), cell_index_img.shape[1]))
+            z_slice = slice(max(1, -10+min(z_indices)), min(10+max(z_indices), cell_index_img.shape[0]))
 
+            cropped_nuc_im = nuc_original_img[z_slice, y_slice, x_slice]
+            # crop and mask the whole cell segmentation
+            cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice]
+            cropped_cell_seg_mask = cropped_cell_seg == cell_value
+            cropped_cell_seg[cropped_cell_seg_mask == False] = 0
+            # crop and mask the nuclear segmentation
+            cropped_nuc_seg = nuc_index_img[z_slice, y_slice, x_slice]
+            cropped_nuc_seg_mask = cropped_nuc_seg == cell_value
+            cropped_nuc_seg[cropped_nuc_seg_mask == True] = 0
+            # this is a 3D Gaussian filter with size of [41, 41, 11]
+            # corresponds to a sigma array of [41, 41, 11]/(4*sqrt(2*log(2)))
+            filter_correction = gaussian_filter(cropped_cell_seg, [41, 41, 11])
+            cropped_filtered_nuc_im = np.divide(gaussian_filter(cropped_nuc_im, [41, 41, 11]), filter_correction)
+            crop_max = cropped_filtered_nuc_im.max()
+            crop_min = cropped_filtered_nuc_im.min()
 
+            im_threshold = np.multiply(cropped_filtered_nuc_im[cropped_filtered_nuc_im > 1.1], threshold_otsu(cropped_filtered_nuc_im[cropped_filtered_nuc_im > 0]))
