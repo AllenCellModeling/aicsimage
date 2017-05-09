1b1c21bb2db82196747bfe1aeaa480def3fbf0b9
evanw <evanw@alleninstitute.org>
2017-05-09 09:38:38 -0700

    Added n-dimensional functionality and modified unit tests

--- aicsimage/processing/imgCenter.py
+++ aicsimage/processing/imgCenter.py
@@ -42,21 +42,28 @@ def _shape_to(img, out_shape, val=0):
     return out
 
 
-def crop_all(images):
+def crop_all(images, axis=(-3, -2, -1)):
     """
     Crop all images by the same amount. The amount to crop will be calculated
     so that an equal amount is removed on both sides of each axis to keep the center of mass
     in the center
+    :param images: List of images to crop. The images can be any shape or dimensionality but must
+    all have the same shape
+    :param axis: List of axis to crop along. Default is the last three axis (meant to correspond to ZYX)
+    :return: List of cropped images, in the same order as they were passed in
     """
     try:
+        if not isinstance(images, (type(tuple()), type([]))):
+            raise ValueError("images must be an iterable")
         shape = images[0].shape
-        if len(shape) != 4:
-            raise ValueError("images must be a list of 4d numpy arrays")
-        edges = [get_edges(img) for img in images]
+        if not all(img.shape == shape for img in images):
+            raise ValueError("images must all have the same shape")
+        edges = [get_edges(img, axis=axis) for img in images]
     except (IndexError, AttributeError, TypeError):
-        raise ValueError("images must be a list of 4d numpy arrays")
-    slices = [slice(None, None)]
-    for axis_index, axis_edges in enumerate(zip(*edges), start=1):
+        raise ValueError("images must be a list of numpy arrays")
+    slices = [[None, None] for _ in range(len(shape))]
+    for axis_index, axis_edges in zip(axis, zip(*edges)):
+        axis_length = shape[axis_index]
         # axis_edges is a list of 2 element lists containing the endpoints
         # for each image in a single axis
         axis_length = shape[axis_index]
@@ -64,35 +71,39 @@ def crop_all(images):
         axis_max = max(a[1] for a in axis_edges)
         # crop each side by the same amount
         slice_amount = min(axis_min, axis_length - axis_max)
-        slices.append(slice(slice_amount, axis_length - slice_amount))
-    return [img[slices] for img in images]
+        slices[axis_index] = [slice_amount, axis_length - slice_amount]
+    return [img[tuple(slice(*s) for s in slices)] for img in images]
 
 
-def center_image(image, moves=None, fill=0):
+def center_image(image, moves=None, axis=(-3, -2, -1), fill=0):
     """
     Aligns image based on the center of mass.
-    :param image: CZYX image as a numpy array that will be centered by adding padding
-    :param moves: tuple of integers, returned from a previous call of this function. If specified, will move the image by
+    :param image: Image as a numpy array that will be centered by adding padding. Can be any dimensionality
+    :param moves: List of integers, returned from a previous call of this function. If specified, will move the image by
     that amount instead of recalculating it (used to move multiple images by the same amount)
+    :param axis: Iterable containing the axis to center the image on. Order does not affect the output. Default is the last three
+    axis (meant to be ZYX)
     :param fill: Value to use when adding padding. Default is 0
     :return: If moves is not specified, a tuple containing the centered image and the moves tuple to be provided to
     future function calls. If moves is specified, it returns just the moved image
     """
-    try:
-        if image.ndim != 4:
-            raise ValueError("image must be a 4d numpy array")
-    except AttributeError:
+    if not isinstance(image, np.ndarray):
         raise ValueError("image must be a 4d numpy array")
     return_tuple = moves is None
     if moves is None:
         # calculate how far off center the image is in each direction
-        # slice with [1:] to skip the color axis
-        moves = tuple(int(m) - (l // 2) for l, m in zip(image.shape[1:], center_of_mass(image)[1:]))
+        com = center_of_mass(image)
+        moves = [0] * image.ndim
+        try:
+            for a in axis:
+                moves[a] = int(com[a]) - (image.shape[a] // 2)
+        except IndexError:
+            raise ValueError("Invalid axis")
     # add padding to image to put center of mass in center
-    padding = [(0, 0)] + [(-p, 0) if p < 0 else (0, p) for p in moves]
-    if not isinstance(moves, tuple):
-        raise ValueError("Incorrect format for 'moves', should be tuple returned from previous function call")
+    padding = [(-p, 0) if p < 0 else (0, p) for p in moves]
+    if not isinstance(moves, list):
+        raise ValueError("Incorrect format for 'moves', should be list returned from previous function call")
     if return_tuple:
-        return (np.pad(image, padding, "constant"), moves)
+        return (np.pad(image, padding, "constant", constant_values=fill), moves)
     else:
-        return np.pad(image, padding, "constant")
+        return np.pad(image, padding, "constant", constant_values=fill)
