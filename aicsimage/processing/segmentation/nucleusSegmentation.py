626c307e4b32386f94dc87c381a44d7558ffdb59
zacharyc <zacharyc@alleninstitute.org>
2017-05-11 12:00:51 -0700

    add fixes to filter correction and attempt keep connected components

--- aicsimage/processing/segmentation/nucleusSegmentation.py
+++ aicsimage/processing/segmentation/nucleusSegmentation.py
@@ -3,13 +3,35 @@
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 from skimage.filters import threshold_otsu
-from skimage.morphology import opening, disk
+from scipy.ndimage.measurements import label
+from skimage.measure import regionprops
+from skimage import morphology
+from aicsimage.io.omeTifWriter import OmeTifWriter
 
-def fill_nucleus_segmentation(cell_index_img, nuc_index_img, nuc_original_img):
+def keep_connected_components(image, low_threshold, high_threshold=None):
+
+    if high_threshold is None:
+        high_threshold = len(image)
+
+    labels = label(image)
+    object_areas = [ob_region.area for ob_region in regionprops(labels[0])]
+    output = np.zeros(image.shape)
+
+    if len(object_areas) > 1:
+        indices = np.where(low_threshold < object_areas <= high_threshold)
+        for index in indices:
+            output = output or labels[0] == index
+    else:
+        output = image.copy()
+
+    return output
+
+
+
+def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
     """
     This function is built to fill in the holes of the nucleus segmentation channel
     :param cell_index_img: A ZYX ndarray - represents the segmented image of all cell bodies
-    :param nuc_index_img: A ZYX ndarray - represents the segmented image of all nuclei
     :param nuc_original_img: A ZYX ndarray - represents the original image of the nuclei channel
     :return: corrected_image: A ZYX ndarray - represents a corrected segmented image of the nuclei (all holes filled in)
     """
@@ -31,25 +53,45 @@ def fill_nucleus_segmentation(cell_index_img, nuc_index_img, nuc_original_img):
             y_slice = slice(max(1, -10+min(y_indices)), min(10+max(y_indices), cell_index_img.shape[1]))
             z_slice = slice(max(1, -10+min(z_indices)), min(10+max(z_indices), cell_index_img.shape[0]))
 
-            cropped_nuc_im = nuc_original_img[z_slice, y_slice, x_slice]
             # crop and mask the whole cell segmentation
             cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice].astype(np.float64)
-            cropped_cell_seg[cropped_cell_seg == cell_value] = 0
-            # crop and mask the nuclear segmentation
-            cropped_nuc_seg = nuc_index_img[z_slice, y_slice, x_slice].astype(np.float64)
-            cropped_nuc_seg[cropped_nuc_seg == cell_value] = 0
+            cropped_cell_seg[cropped_cell_seg != cell_value] = 0
+            # OmeTifWriter("./img/segmentation/cell_seg.ome.tif", overwrite_file=True).save(cropped_cell_seg)
+
+            output = nuc_original_img[z_slice, y_slice, x_slice]
+            # OmeTifWriter("./img/segmentation/cropped.ome.tif", overwrite_file=True).save(output)
+
+            # mask the nuclear light image
+            output[cropped_cell_seg != cell_value] = 0
+            # OmeTifWriter("./img/segmentation/masked_original.ome.tif", overwrite_file=True).save(output)
+
             # this is a 3D Gaussian filter with size of [41, 41, 11]
             # corresponds to a sigma array of [41, 41, 11]/(4*sqrt(2*log(2)))
             filter_correction = gaussian_filter(cropped_cell_seg, [41, 41, 11])
-            cropped_filtered_nuc_im = gaussian_filter(cropped_nuc_im, [41, 41, 11])
-            # this indexing assures that no values in cropped_filtered_nuc_im are divided by zero
-            cropped_filtered_nuc_im[filter_correction > 0] /= filter_correction[filter_correction > 0]
+            OmeTifWriter("./img/segmentation/correction.ome.tif", overwrite_file=True).save(filter_correction)
+            output = gaussian_filter(output, [41, 41, 11])
+            OmeTifWriter("./img/segmentation/filtered.ome.tif", overwrite_file=True).save(output)
 
-            otsu_threshold = threshold_otsu(cropped_filtered_nuc_im[cropped_filtered_nuc_im > 0])
-            cropped_filtered_nuc_im[cropped_filtered_nuc_im > 1.1] *= otsu_threshold
+            # this indexing assures that no values in output are divided by zero
+            output[filter_correction > 0] /= filter_correction[filter_correction > 0]
+            OmeTifWriter("./img/segmentation/filter_corrected.ome.tif", overwrite_file=True).save(output)
 
-            
+            otsu_threshold = threshold_otsu(output[output > 0])
+            output[output <= otsu_threshold] = 0
+            output[output > otsu_threshold] = cell_value
+            OmeTifWriter("./img/segmentation/threshold.ome.tif", overwrite_file=True).save(output)
 
+            output = output.astype(np.int)
 
+            output = morphology.remove_small_objects(output)
+            # OmeTifWriter("./img/segmentation/opening.ome.tif", overwrite_file=True).save(output)
+
+            # fill holes for each slice
+            # for z in range(output.shape[0]):
+            #     output[z, binary_fill_holes(output[z])] = 1
+            output = morphology.remove_small_holes(output)
+            # OmeTifWriter("./img/segmentation/slices_filled.ome.tif", overwrite_file=True).save(output)
+            total_area = np.prod(output.shape)
+            output = keep_connected_components(output,  total_area // 4, total_area * 2)
 
 
--- test/testNucleusSegmentation.py
+++ test/testNucleusSegmentation.py
@@ -13,7 +13,6 @@ class NucleusSegmentationTestGroup(unittest.TestCase):
 
     def test_Segmentation(self):
         cell_index_im = TifReader("img/segmentation/input_1_cellWholeIndex.tiff").load()
-        nuc_index_im = TifReader("img/segmentation/input_2_nucWholeIndex.tiff").load()
         original_im = TifReader("img/segmentation/input_3_nuc_orig_img.tiff").load()
 
-        image = nucleusSegmentation.fill_nucleus_segmentation(cell_index_im, nuc_index_im, original_im)
+        image = nucleusSegmentation.fill_nucleus_segmentation(cell_index_im, original_im)
