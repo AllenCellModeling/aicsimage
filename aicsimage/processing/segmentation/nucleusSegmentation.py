00a18504b97b703b6b2fdd319897d92890232a3a
zacharyc <zacharyc@alleninstitute.org>
2017-05-15 09:43:26 -0700

    finish removing small holes and objects

--- aicsimage/processing/segmentation/nucleusSegmentation.py
+++ aicsimage/processing/segmentation/nucleusSegmentation.py
@@ -46,52 +46,36 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
         # if a cell exists with this cell_value
         if len(cell_indices[0]) > 0:
             z_indices, y_indices, x_indices = cell_indices[0], cell_indices[1], cell_indices[2]
-
             # creates buffer of 10 pixels/slices around the cell, or stops at the boundaries of the original image
             x_slice = slice(max(1, -10+min(x_indices)), min(10+max(x_indices), cell_index_img.shape[2]))
             y_slice = slice(max(1, -10+min(y_indices)), min(10+max(y_indices), cell_index_img.shape[1]))
             z_slice = slice(max(1, -10+min(z_indices)), min(10+max(z_indices), cell_index_img.shape[0]))
-
             # crop and mask the whole cell segmentation
             cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice].astype(np.float64)
             cropped_cell_seg[cropped_cell_seg != cell_value] = 0
-            # OmeTifWriter("./img/segmentation/cell_seg.ome.tif", overwrite_file=True).save(cropped_cell_seg)
-
+            # crop and mask the nucleus channel
             output = nuc_original_img[z_slice, y_slice, x_slice]
-            # OmeTifWriter("./img/segmentation/cropped.ome.tif", overwrite_file=True).save(output)
-
-            # mask the nuclear light image
             output[cropped_cell_seg != cell_value] = 0
-            # OmeTifWriter("./img/segmentation/masked_original.ome.tif", overwrite_file=True).save(output)
-
-            # this is a 3D Gaussian filter with size of [41, 41, 11]
-            # corresponds to a sigma array of [41, 41, 11]/(4*sqrt(2*log(2)))
-            filter_correction = gaussian_filter(cropped_cell_seg[cropped_cell_seg > 0], 41)
-            cropped_cell_seg[cropped_cell_seg > 0] = filter_correction
-            OmeTifWriter("./img/segmentation/correction.ome.tif", overwrite_file=True).save(cropped_cell_seg)
-
+            # filter the membrane segmentation channel
+            cropped_cell_seg[cropped_cell_seg > 0] = gaussian_filter(cropped_cell_seg[cropped_cell_seg > 0], 41)
+            # filter the nuclear channel
             output = gaussian_filter(output, [41, 41, 11])
-            OmeTifWriter("./img/segmentation/filtered.ome.tif", overwrite_file=True).save(output)
-
             # this indexing assures that no values in output are divided by zero
             output[cropped_cell_seg > 0] /= cropped_cell_seg[cropped_cell_seg > 0]
             output[cropped_cell_seg == 0] = 0
-            OmeTifWriter("./img/segmentation/filter_corrected.ome.tif", overwrite_file=True).save(output)
-
+            # threshold and mask to get the new nuclear segmentation
             otsu_threshold = threshold_otsu(output[output > 0])
             output[output <= otsu_threshold] = 0
-            output[output > otsu_threshold] = cell_value
+            output[output > otsu_threshold] = 1
             OmeTifWriter("./img/segmentation/threshold.ome.tif", overwrite_file=True).save(output)
-            output = output.astype(np.int)
-            output = morphology.remove_small_objects(output)
-            OmeTifWriter("./img/segmentation/opening.ome.tif", overwrite_file=True).save(output)
 
-            # fill holes for each slice
-            # for z in range(output.shape[0]):
-            #     output[z, binary_fill_holes(output[z])] = 1
-            output = morphology.remove_small_holes(output)
-            OmeTifWriter("./img/segmentation/slices_filled.ome.tif", overwrite_file=True).save(output)
-            # total_area = np.prod(output.shape)
-            # output = keep_connected_components(output,  total_area // 4, total_area * 2)
+            # clean the images of objects and holes
+            for z in range(output.shape[0]):
+                output[z] = morphology.remove_small_objects(output[z].astype(np.int))
+                output[z] = morphology.remove_small_holes(output[z].astype(np.int))
+            OmeTifWriter("./img/segmentation/removal.ome.tif", overwrite_file=True).save(output)
+
+            total_area = np.prod(output.shape)
+            output = keep_connected_components(output,  total_area // 4, total_area * 2)
 
 
