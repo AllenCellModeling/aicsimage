8b18e5436af426033433d54e76a7b21606df18f7
zacharyc <zacharyc@alleninstitute.org>
2017-05-17 10:40:05 -0700

    proper output with debugging intermediate images

--- aicsimage/processing/segmentation/nucleusSegmentation.py
+++ aicsimage/processing/segmentation/nucleusSegmentation.py
@@ -1,5 +1,5 @@
 # author: Zach Crabtree zacharyc@alleninstitute.org
-
+from __future__ import print_function
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 from skimage.filters import threshold_otsu
@@ -50,31 +50,36 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
     nuc_original_img = (nuc_original_img - original_min) / (original_max - original_min) * 255
     total_out = np.zeros(nuc_original_img.shape)
 
-    for cell_value in range(1, cell_index_img.max()):
+    for cell_value in range(1, cell_index_img.max() + 1):
         # get indices of cell with cell_value as its ID
         cell_indices = np.where(cell_index_img == cell_value)
         # if a cell exists with this cell_value
         if len(cell_indices[0]) > 0:
+            print("Processing cell label" + str(cell_value) + "...", end="")
             z_indices, y_indices, x_indices = cell_indices[0], cell_indices[1], cell_indices[2]
             # creates buffer of 10 pixels/slices around the cell, or stops at the boundaries of the original image
             x_slice = slice(max(1, -10+min(x_indices)), min(10+max(x_indices), cell_index_img.shape[2]))
             y_slice = slice(max(1, -10+min(y_indices)), min(10+max(y_indices), cell_index_img.shape[1]))
             z_slice = slice(max(1, -10+min(z_indices)), min(10+max(z_indices), cell_index_img.shape[0]))
             # crop and mask the whole cell segmentation
-            cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice].astype(np.float64)
+            cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice].astype(np.float64).copy()
             cropped_cell_seg[cropped_cell_seg != cell_value] = 0
-            _save_inter_image(cropped_cell_seg, cell_value, "cropped_cell_seg")
+            # _save_inter_image(cropped_cell_seg, cell_value, "cropped_cell_seg")
             # crop and mask the nucleus channel
-            output = nuc_original_img[z_slice, y_slice, x_slice]
+            output = nuc_original_img[z_slice, y_slice, x_slice].copy()
             output[cropped_cell_seg != cell_value] = 0
+            _save_inter_image(output, cell_value, "cropped_nuc")
             # filter the membrane segmentation channel
             cropped_cell_seg[cropped_cell_seg > 0] = gaussian_filter(cropped_cell_seg[cropped_cell_seg > 0], 41)
+
             # filter the nuclear channel
             output = gaussian_filter(output, [41, 41, 11])
+            _save_inter_image(output, cell_value, "filtered")
             # this indexing assures that no values in output are divided by zero
             output[cropped_cell_seg > 0] /= cropped_cell_seg[cropped_cell_seg > 0]
             output[cropped_cell_seg == 0] = 0
-            # threshold and mask to get the new nuclear segmentation\
+            _save_inter_image(output, cell_value, "corrected")
+            # threshold and mask to get the new nuclear segmentation
             if len(output[output > 0]) > 0:
                 otsu_threshold = threshold_otsu(output[output > 0])
                 output[output <= otsu_threshold] = 0
@@ -83,7 +88,7 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
 
                 # clean the images of objects and holes
                 for z in range(output.shape[0]):
-                    image_slice = output[z].astype(np.bool)
+                    image_slice = output[z].astype(np.int)
                     output[z] = morphology.remove_small_objects(image_slice)
                     output[z] = morphology.remove_small_holes(image_slice)
                 _save_inter_image(output, cell_value, "removal")
@@ -91,9 +96,11 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
                 total_volume = np.prod(output.shape)
                 output = keep_connected_components(output,  total_volume // 4, total_volume * 2)
                 _save_inter_image(output, cell_value, "connected_components")
-            # change boolean value back to original segmentation value
-            output[output == 1] = cell_value
-            total_out[z_slice, y_slice, x_slice] = output
+                # change boolean value back to original segmentation value
+                output *= cell_value
+                total_out[z_slice, y_slice, x_slice] += output
+                _save_inter_image(total_out, cell_value, "total_out")
+            print("done")
 
     _save_inter_image(total_out, 0, "total_out")
 
