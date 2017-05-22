f5d401aa7180e03addfe28fd0bd01140eb6c17ef
zacharyc <zacharyc@alleninstitute.org>
2017-05-22 12:12:44 -0700

    mask nucleus segmentation to inside the membrane segmentation

--- aicsimage/processing/segmentation/nucleusSegmentation.py
+++ aicsimage/processing/segmentation/nucleusSegmentation.py
@@ -52,9 +52,8 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
     nuc_original_img = (nuc_original_img - original_min) / (original_max - original_min) * 255
     total_out = np.zeros(nuc_original_img.shape)
 
-    # for cell_value in range(1, cell_index_img.max() + 1):
-    for cell_value in range(1, 5):
-
+    for cell_value in range(1, cell_index_img.max() + 1):
+    # for cell_value in range(1, 5):
         # get indices of cell with cell_value as its ID
         cell_indices = np.where(cell_index_img == cell_value)
         # if a cell exists with this cell_value
@@ -69,10 +68,11 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
             # crop and mask the whole cell segmentation
             cropped_cell_seg = cell_index_img[z_slice, y_slice, x_slice].astype(np.float64).copy()
             cropped_cell_seg[cropped_cell_seg != cell_value] = 0
+            nucleus_mask = cropped_cell_seg.copy()
             # _save_inter_image(cropped_cell_seg, cell_value, "cropped_cell_seg")
             # crop and mask the nucleus channel
             output = nuc_original_img[z_slice, y_slice, x_slice].copy()
-            output[cropped_cell_seg != cell_value] = 0
+            output[nucleus_mask != cell_value] = 0
             # _save_inter_image(output, cell_value, "cropped_nuc")
             # filter the membrane segmentation channel
             sigma = np.divide([41, 41, 11], (4*m.sqrt(2*m.log(2))))
@@ -93,6 +93,7 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
 
                 # clean the images of objects and holes
                 output = morphology.remove_small_objects(output.astype(np.int))
+                # TODO does this next call have to recast output as int?
                 output = morphology.remove_small_holes(output.astype(np.int))
                 # output needs to be recast as int, because the morphology methods above return boolean arrays
                 output = output.astype(np.int)
@@ -103,13 +104,8 @@ def fill_nucleus_segmentation(cell_index_img, nuc_original_img):
                 # _save_inter_image(output, cell_value, "connected_components")
                 # change boolean value back to original segmentation value
                 output *= cell_value
-                # for z in range(z_slice.start, z_slice.stop):
-                #     for y in range(y_slice.start, y_slice.stop):
-                #         for x in range(x_slice.start, x_slice.stop):
-                #             px_weight = output[z-z_slice.start, y-y_slice.start, x-x_slice.start]
-                #             if px_weight != 0:
-                #                 total_out[z, y, x] = px_weight
-
+                # mask the nucleus inside the cell membrane segmentation again
+                output[nucleus_mask != cell_value] = 0
                 total_out[z_slice, y_slice, x_slice] += output
                 _save_inter_image(total_out, cell_value, "total_out")
             elapsed_time = timeit.default_timer() - start_time
